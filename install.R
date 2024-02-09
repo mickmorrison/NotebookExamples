0fbb9ee7febbf0026c52b79973ec8a3e0e00255f
Michael Morrison <mmorrisonau@gmail.com>
2024-02-10 06:07:38 +1100

    Update install.R

--- install.R
+++ install.R
@@ -2,6 +2,3 @@
 install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("sf")
-
-# You can also use a single install.packages call with a vector of package names for efficiency
-install.packages(c("dplyr", "ggplot2", "sf"))
